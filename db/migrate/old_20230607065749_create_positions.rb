4b4b02c57c065cfa18cf12a1c11a5af08860ac39
alvink-ukirama <alvin.kosman@ukirama.com>
2023-06-16 09:37:40 +0700

    Delete all old migrations

--- db/migrate/old_20230607065749_create_positions.rb
+++
@@ -1,9 +0,0 @@
-class CreatePositions < ActiveRecord::Migration[6.1]
-  def change
-    create_table :positions do |t|
-      t.string :code_name, limit: 50, unique: true
-
-      t.timestamps
-    end
-  end
-end
